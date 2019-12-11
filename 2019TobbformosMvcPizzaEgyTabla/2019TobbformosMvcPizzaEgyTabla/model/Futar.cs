8fc849215edbcd18b68a96fa86c04557652bc515
Marton Zoltan <martonzoli99@gmail.com>
2019-12-11 13:14:25 +0100

    futar.cs megalkotása

--- 2019TobbformosMvcPizzaEgyTabla/2019TobbformosMvcPizzaEgyTabla/model/Futar.cs
+++ 2019TobbformosMvcPizzaEgyTabla/2019TobbformosMvcPizzaEgyTabla/model/Futar.cs
@@ -10,7 +10,80 @@ namespace _2019TobbformosMvcPizzaEgyTabla.model
     {
         private int id;
         private string name;
-        private string telefomszam;
+        private int phonenumber;
+
+        public Futar(int id, string name, int phonenumber)
+        {
+            this.id = id;
+            this.name = name;
+            this.phonenumber = phonenumber;
+        }
+
+        public Futar(int id, string name, string phonenumber)
+        {
+            this.id = id;
+            if (!isValidName(name))
+                throw new ModelFutarNotValidNameException("A futár neve nem megfelelő!");
+            if (isValidPhonenumber(phonenumber))
+                throw new ModelPizzaNotValidPhonenumberException("A futár telefonszáma nem megfelelő!");
+            this.name = name;
+            this.phonenumber = Convert.ToInt32(phonenumber);
+        }
+
+        public void update (Futar modified)
+        {
+            this.name = modified.getName();
+            this.phonenumber = modified.getPhonenumber();
+        }
+        private bool isValidPhonenumber(string phonenumber)
+        {
+            int eredmeny = 0;
+            if (int.TryParse(phonenumber, out eredmeny))
+                return true;
+            else
+                return false;
+        }
+        private bool isValidName(string name)
+        {
+            if (name == string.Empty)
+                return false;
+            if (!char.IsUpper(name.ElementAt(0)))
+                return false;
+            for (int i = 1; i < name.Length; i = i + 1)
+                if (
+                    !char.IsLetter(name.ElementAt(i))
+                        &&
+                    (!char.IsWhiteSpace(name.ElementAt(i)))
+
+                    )
+                    return false;
+            return true;
+        }
+
+        public void setID(int id)
+        {
+            this.id = id;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public void setPhonenumber(int phonenumber)
+        {
+            this.phonenumber = phonenumber;
+        }
+        public int getId()
+        {
+            return id;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public int getPhonenumber()
+        {
+            return phonenumber;
+        }
 
     }
 }
---
+++ 2019TobbformosMvcPizzaEgyTabla/2019TobbformosMvcPizzaEgyTabla/model/ModelFutarNotValidNameException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace _2019TobbformosMvcPizzaEgyTabla.model
+{
+    [Serializable]
+    internal class ModelFutarNotValidNameException : Exception
+    {
+        public ModelFutarNotValidNameException()
+        {
+        }
+
+        public ModelFutarNotValidNameException(string message) : base(message)
+        {
+        }
+
+        public ModelFutarNotValidNameException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ModelFutarNotValidNameException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
