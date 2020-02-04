db554ca1a0d066d80e33b2d0c48751b9926b763e
Marton Zoltan <martonzoli99@gmail.com>
2020-02-04 13:56:51 +0100

    Form orders complete

--- 2019TobbformosMvcPizzaTobbTabla/TobbformosMvcPizzaTobbTabla/TobbformosMvcPizzaTobbTabla/FormOrders.cs
+++ 2019TobbformosMvcPizzaTobbTabla/TobbformosMvcPizzaTobbTabla/TobbformosMvcPizzaTobbTabla/FormOrders.cs
@@ -14,35 +14,63 @@ namespace TobbformosMvcPizzaTobbTabla
 {
     public partial class FormPizzaFutarKft : Form
     {
-        DataGridView dgv = new DataGridView();
+        DataGridView dgv;
+
         private void fillComboBoxCustomer()
         {
             comboBoxCustomer.DataSource = null;
-            comboBoxCustomer.DataSource = repo.getCustumersName();
+            comboBoxCustomer.DataSource=repo.getCustumersName();
         }
 
-        private void createDinamicDataGridViewOrders()
+        private void createDinamicDataGridViewToOrders()
         {
+            if (dgv != null)
+                return;
+            dgv = new DataGridView();
+
             DataGridViewComboBoxColumn cbc = new DataGridViewComboBoxColumn();
             cbc.Name = "pizzaName";
-            cbc.HeaderText = "Pizza név";
+            cbc.HeaderText = "      Pizza név    ";
             cbc.MaxDropDownItems = 5;
-            cbc.DataSource = repo.getPizzas();
+            cbc.DataSource = repo.getPizzasName();
+            cbc.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;            
+
 
             DataGridViewButtonColumn bcPlusz = new DataGridViewButtonColumn();
+            bcPlusz.UseColumnTextForButtonValue = true;
             bcPlusz.HeaderText = "Mennyiség növelés";
             bcPlusz.Name = "bcPlusz";
             bcPlusz.Text = "+";
+            bcPlusz.AutoSizeMode =DataGridViewAutoSizeColumnMode.AllCells;
 
+            DataGridViewButtonColumn bcMinusz = new DataGridViewButtonColumn();
+            bcMinusz.UseColumnTextForButtonValue = true;
+            bcMinusz.HeaderText = "Mennyiség csökkentés";
+            bcMinusz.Name = "bcMinusz";
+            bcMinusz.Text = "-";
+            bcMinusz.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
 
-            dgv.Location = new Point(30, 50);
+            DataGridViewTextBoxColumn tbcMennyiseg = new DataGridViewTextBoxColumn();
+            tbcMennyiseg.HeaderText = "Mennyiség";
+            tbcMennyiseg.Name = "mennyiseg";
+            tbcMennyiseg.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
+
+            DataGridViewTextBoxColumn tbcEgysegar = new DataGridViewTextBoxColumn();
+            tbcEgysegar.HeaderText = "Egységár";
+            tbcEgysegar.Name = "egysegar";
+            tbcEgysegar.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
+
+            dgv.Location = new Point(30, 60);
+            dgv.Width = 600;
 
             dgv.Columns.Add(cbc);
-
             dgv.Columns.Add(bcPlusz);
+            dgv.Columns.Add(bcMinusz);
+            dgv.Columns.Add(tbcMennyiseg);
+            dgv.Columns.Add(tbcEgysegar);
 
             tabPageMegrendeles.Controls.Add(dgv);
-            //tabPageMegrendeles.Refresh();
-        }
+
+      }
     }
 }
